72a94982d270259ff59d89554e232c1ec504a28c
Slawek <grzebyk.slawomir@gmail.com>
2021-06-04 17:56:52 +0200

    create user on DB

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,4 +1,4 @@
-const express = require("expres");
+const express = require("express");
 const router = express.Router();
 // const mongoose = require('mongoose');
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const User = require('../models/users')
+const User = require("../models/users");
 
 router.get("/signup", (req, res, next) => {
-  console.log("request on /signup", req.body);
-
   const user = new User({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    password: req.body.password
+    password: req.body.password,
+    comment: req.body.comment
+  });
 
-  })
-
-  return res.status(200).json({wiadomosc: user})
-
-
-
+  user.save()
+      .then(()=>{
+        res.status(201).json({textMessage: "Utworzono użytkownia"})
+        res.status(500).json({textMessage: "Blad Servera"})
+      })
 });
-
-
 
 module.exports = router;
